8521a1fd084e5eca503b0538306cc9bd76c2a0b9
Brigitte Huneke <brigittehuneke@gmail.com>
2016-11-11 14:18:19 -0800

    problem 2 passed

--- app.js
+++ app.js
@@ -16,12 +16,11 @@ function sum(a,b){ //eslint-disable-line*/
 function sum(a, b){
   var total = (a + b);
   var test = [total, 'The sum of ' + a + ' and ' + b + ' is ' + total + '.'];
-  console.log(a);
-  console.log(b);
-  console.log(total);
+  console.log('The value of a is: ' + a);
+  console.log('The value of b is: ' + b);
+  console.log('The sum of a and b is: ' + total);
   console.log(test);
   return test;
-
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -32,19 +31,28 @@ testSum(4, 7);
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiply() that takes in two numbers as arguments and returns an array where
+//the first element is the product of those numbers, and the second element is a string that EXACTLY
+//follows this example and uses the values that were input into the function:
 
 "The product of 5 and 9 is 45."
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment
+//the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a,b){ //eslint-disable-line
-
+function multiply(a, b){ //eslint-disable-line
+  var productAnswer = (a * b);
+  var testProblemTwo = [productAnswer, 'The product of ' + a + ' and ' + b + ' is ' + productAnswer + '.'];
+  console.log('The value of a is: ' + a);
+  console.log('The value of b is: ' + b);
+  console.log('The product of a and b is: ' + productAnswer);
+  console.log(testProblemTwo);
+  return testProblemTwo;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
